af9a0deccf3c77c6237ca16a07ace95810f2ea4d
Optional-Josh <caguimbaljoshu21@gmail.com>
2024-11-14 14:42:52 +0800

    version 0.3

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-from details import main_menu
+from record import Record
+from file_manipulation import write_into_file, read_file
+
+
+# function that asks for user choices and serves as a main menu
+def main_menu():
+    while True:
+        user_input = input("Type y to enter a record for your finances or q to quit the menu: ")
+        if user_input.lower() == "y":
+            # record = record_log()
+            write_into_file()
+            read_file()
+        elif user_input.lower() == "q":
+            break
+        else:
+            print("Please input from choices given only")
 
 # serves as the main python file, which will run the other python modules
 if __name__ == "__main__":
-    main_menu()
+    record_one = Record()
+    record_two = Record()
+    record_one
+    record_two
+
+    
