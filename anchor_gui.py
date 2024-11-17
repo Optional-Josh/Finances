5e4adb207dc7ced82abc7cfb85420247b9ffe9b9
Optional-Josh <caguimbaljoshu21@gmail.com>
2024-11-17 08:41:37 +0800

    version 0.8 added

--- anchor_gui.py
+++ anchor_gui.py
@@ -11,10 +11,13 @@ class Top_Frame(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent) # inherits methods from ctk.CTkFrame and sets the master for the frame from parent parameter
 
-
         # creating widgets
-        self.label = ctk.CTkLabel(self, text="LMAO LAMo", wraplength=500, justify="center", anchor="center", font=("Courier", 15))
+        self.label = ctk.CTkLabel(self, text="Welcome", wraplength=700, justify="center", anchor="center", font=("Courier", 20), height=500, width=500)
         self.label.pack(expand=True, fill="both")
+
+        # configure customization
+        self.configure(fg_color="#344e41")
+        self.configure(fg_color="transparent")
 
         # pack frame itself
         self.pack(side="top", expand=True, fill="both")
@@ -50,12 +53,21 @@ class Bottom_Frame(ctk.CTkFrame):
 
         # creating widgets for first/left container
         first_button_container = ctk.CTkFrame(self.first_container)
-        ctk.CTkLabel(first_button_container, text="Main Menu").pack(pady=10, ipady=5)
+        ctk.CTkLabel(first_button_container, text="Main Menu", anchor="s", fg_color="transparent", font=("Arial",20,"bold")).pack(pady=10, ipady=5)
 
         self.button_record = ctk.CTkButton(first_button_container, text="Add a Record", command=self.add_record)
         self.button_read = ctk.CTkButton(first_button_container, text="Read Records", command=self.read_json_content)
         self.button_data = ctk.CTkButton(first_button_container, text="Data Analytics")
         self.button_quit = ctk.CTkButton(first_button_container, text="Quit", command=self.quit_app)
+
+        # customization by configuration of widgets for first container
+        self.first_container.configure(fg_color="#3a5a40")
+        first_button_container.configure(fg_color="transparent")
+
+        self.button_record.configure(fg_color="#003049", hover_color="#f77f00", text_color_disabled="#f07167")
+        self.button_read.configure(fg_color="#003049", hover_color="#f77f00", text_color_disabled="#f07167")
+        self.button_data.configure(fg_color="#003049", hover_color="#f77f00", text_color_disabled="#f07167")
+        self.button_quit.configure(fg_color="#003049", hover_color="#f77f00", text_color_disabled="#f07167")
 
         # packing widgets of first container
         self.button_record.pack(pady=5)
@@ -80,8 +92,17 @@ class Bottom_Frame(ctk.CTkFrame):
         self.add_amount = ctk.CTkButton(self.second_container, state="disabled", text="Add Amount", command=self.add_amount_input)
         self.submit_records = ctk.CTkButton(self.second_container, state="disabled", text="Submit Records and Save to JSON", command=self.return_records)
 
+        # customization by configuration of widgets for second container
+        self.second_container.configure(fg_color="#588157")
+
+        self.add_date.configure(fg_color="#003049", hover_color="#f77f00", text_color_disabled="#f07167")
+        self.add_category.configure(fg_color="#003049", hover_color="#f77f00", text_color_disabled="#f07167")
+        self.add_description.configure(fg_color="#003049", hover_color="#f77f00", text_color_disabled="#f07167")
+        self.add_amount.configure(fg_color="#003049", hover_color="#f77f00", text_color_disabled="#f07167")
+        self.submit_records.configure(fg_color="#003049", hover_color="#f77f00", text_color_disabled="#f07167")
+
         # pack widgets of second container
-        self.record_label = ctk.CTkLabel(self.second_container, text="Add a Record")
+        self.record_label = ctk.CTkLabel(self.second_container, text="Add a Record", font=("Arial",20,"bold"))
         ctk.CTkLabel(self.second_container, text="Joshua Caguimbal").place(relx=0.99, rely=1, anchor="se")
 
         # place method for widgets
@@ -94,10 +115,10 @@ class Bottom_Frame(ctk.CTkFrame):
         self.add_category.place(relx=0.60, rely=0.40)
         self.add_description.place(relx=0.60, rely=0.50)
         self.add_amount.place(relx=0.60, rely=0.60)
-        self.submit_records.place(relx=0.30, rely=0.70)
+        self.submit_records.place(relx=0.30, rely=0.80)
 
         # pack frame containers
-        self.first_container.pack(side="left", expand=True)
+        self.first_container.pack(side="left", expand=True, fill="both")
         self.second_container.pack(side="left", expand=True, fill="both")
 
         # event binds
--- main.py
+++ main.py
@@ -6,7 +6,13 @@ class Main_Window(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Finance Tracker") # set window title name
-        self.geometry("800x800") # set window width and height
+
+        # height and width configuration to ensure when window is created, it is placed in the center of the screen
+        height = 900
+        width = 900
+        x = (self.winfo_screenwidth()//2) - (width//2)
+        y = (self.winfo_screenheight()//2) - (width//2) + (-50)
+        self.geometry("{}x{}+{}+{}".format(width, height, x, y)) # set window width, height and computed values
 
         # calling other frame objects
         self.top_frame = Top_Frame(self) # create top frame object into window
